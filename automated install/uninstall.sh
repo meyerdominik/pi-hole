304fdc14ae267360cd64471a4a7fe39d4acf4fed
Dominik Meyer <meyerdominik@users.noreply.github.com>
2020-03-29 11:09:04 +0200

    Update uninstall.sh

--- automated install/uninstall.sh
+++ automated install/uninstall.sh
@@ -102,8 +102,7 @@ removeAndPurge() {
 removeNoPurge() {
     # Only web directories/files that are created by Pi-hole should be removed
     echo -ne "  ${INFO} Removing Web Interface..."
-    ${SUDO} rm -rf /var/www/html/admin &> /dev/null
-    ${SUDO} rm -rf /var/www/html/pihole &> /dev/null
+    ${SUDO} rm -rf /var/www/html/ &> /dev/null
     ${SUDO} rm -f /var/www/html/index.lighttpd.orig &> /dev/null
 
     # If the web directory is empty after removing these files, then the parent html directory can be removed.
